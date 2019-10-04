2173e32033351d2e1d357dc2efc307e36b39f1ad
Jon O'Drobinak <jodrobinak@gmail.com>
2019-10-04 08:48:02 -0600

    Update App and buttons to test switching between topics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/local';
+import technology from '../../data/technology';
+import science from '../../data/science';
+import health from '../../data/health';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import Menu from '../Menu/Menu';
@@ -7,22 +11,25 @@ import Menu from '../Menu/Menu';
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      local
-    }
+    this.state = {}
   }
 
   deleteArticle(articleId) {
     console.log('idea id is ', articleId);
   }
 
+  sortArticles = (newTopic) => {
+    console.log('newTopic is : ', newTopic);
+    this.setState({ topic: science })
+  }
+
   render () {
-    // console.log('App state is: ', local);
+    console.log('App state is: ', this.state.topic);
     return (
       <div className="app">
         <h1>What's New?</h1>
-        <Menu />
-        <NewsContainer news={this.state.local} deleteArticle={this.deleteArticle} />
+        <Menu sortArticles={this.sortArticles} />
+        {this.state.topic && <NewsContainer news={this.state.topic} deleteArticle={this.deleteArticle} />}
       </div>
     );
   }
